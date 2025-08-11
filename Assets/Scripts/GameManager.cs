8ebf6c71de84fb23562087b44858a329b820ea51
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-08-11 20:12:55 +0300

    Revert "prefab error 2"
    This reverts commit 62b2a78a0eb1b493bd0a60a29b41f960bda34fbf.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -108,7 +108,7 @@ public class GameManager : MonoBehaviour
     public void RestartGame()
     {
         AudioManager.Instance.Play(SoundType.TransitionSound);
-        SceneManager.LoadScene("DefaultMode");
+        SceneManager.LoadScene("SampleScene");
     }
 
 
