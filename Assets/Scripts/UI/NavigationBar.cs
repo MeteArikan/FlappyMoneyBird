a56e3d6053f3fa1e04357cc6ef57ef3f4dbf3edd
Mete Arıkan <82828377+MeteArikan@users.noreply.github.com>
2025-08-22 22:01:46 +0300

    v0.21.1 NavBar Animation Fix

--- Assets/Scripts/UI/NavigationBar.cs
+++ Assets/Scripts/UI/NavigationBar.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+
 public class NavigationBar : MonoBehaviour
 {
     [SerializeField] private Transform[] birdIcons; // Assign in Inspector: 0=Default, 1=Money, 2=Hard
     [SerializeField] private Button _moneyModeButton;
     [SerializeField] private Button _hardModeButton;
     [SerializeField] private Button _defaultModeButton;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
         _moneyModeButton.onClick.AddListener(() => GameManager.Instance.NavigateToGameMode(GameMode.MoneyMode));
         _hardModeButton.onClick.AddListener(() => GameManager.Instance.NavigateToGameMode(GameMode.HardMode));
@@ -20,10 +21,23 @@ public class NavigationBar : MonoBehaviour
         AnimateForCurrentMode();
     }
 
-    // private void OnDisable()
-    // {
-    //     AnimateForCurrentMode();
-    // }
+    private void OnDisable()
+    {
+        // Kill all tweens safely when disabled
+        foreach (var bird in birdIcons)
+        {
+            bird.DOKill();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Double safety: kill tweens if object is destroyed
+        foreach (var bird in birdIcons)
+        {
+            bird.DOKill();
+        }
+    }
 
     private void AnimateForCurrentMode()
     {
@@ -31,45 +45,29 @@ public class NavigationBar : MonoBehaviour
         AnimateSelectedBird(modeIndex);
     }
 
-
     public void AnimateSelectedBird(int modeIndex)
     {
         // Reset all birds to default position/rotation
         foreach (var bird in birdIcons)
         {
+            bird.DOKill(); // Kill old tweens before making new ones
             bird.DOLocalMoveY(0, 0.2f);
             bird.DOLocalRotate(Vector3.zero, 0.2f);
         }
 
         // Animate the selected bird
         birdIcons[modeIndex]
-            .DOLocalMoveY(30f, 0.3f) // Move up by 30 units
+            .DOLocalMoveY(30f, 0.3f)
             .SetEase(Ease.OutBack);
         birdIcons[modeIndex]
             .DOLocalRotate(new Vector3(0, 0, 30f), 0.3f)
             .SetEase(Ease.OutBack);
 
         Debug.Log("Animating bird: " + modeIndex);
-        switch (modeIndex)
-        {
-            case 0:
-                _defaultModeButton.interactable = false;
-                _moneyModeButton.interactable = true;
-                _hardModeButton.interactable = true;
-                break;
-            case 1:
-                _defaultModeButton.interactable = true;
-                _moneyModeButton.interactable = false;
-                _hardModeButton.interactable = true;
-                break;
-            case 2:
-                _defaultModeButton.interactable = true;
-                _moneyModeButton.interactable = true;
-                _hardModeButton.interactable = false;
-                break;
 
-        }
+        // Update button states
+        _defaultModeButton.interactable = modeIndex != 0;
+        _moneyModeButton.interactable = modeIndex != 1;
+        _hardModeButton.interactable   = modeIndex != 2;
     }
-    
-
 }
